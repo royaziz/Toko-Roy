857baf007274bd7d8e3153ecf98d4859b029613e
FlutLab.io <>
2022-05-25 02:52:55 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: BlazeShop(),
+    );
+  }
+}
+
+class BlazeShop extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            title: Text("Blaze Shop"),
+          ),
+        ));
+  }
+}
